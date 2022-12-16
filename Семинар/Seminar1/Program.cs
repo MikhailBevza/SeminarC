da97d0ad071e4af318ef006c5fb16a58bf614c8a
Mikhail <miha.bevza@gmail.com>
2022-12-16 23:17:53 +0300

    Seminar 1

---
+++ Семинар/Seminar1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Семинар 1
+
+// Задача 1. напишите программу, которая на ввод принемает два числа
+/*
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = num2 * num2;
+
+if(num1 == quad2)
+{
+    Console.WriteLine("Yes!");
+}
+else
+{
+    Console.WriteLine("No!");
+}
+*/
+
+// Задача 2.
+
+// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+/*
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current  = -number;
+
+while(current <= number)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
+
+
+
